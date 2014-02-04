762c7e6ab5701d4d6a577d7b83cc3095c3f741a9
Adam Le Doux <aledoux6@gatech.edu>
2014-02-04 14:17:34 -0500

    small changes to player.js

--- player.js
+++ player.js
@@ -32,32 +32,36 @@ function GameInfo(username){
 GameInfo.prototype.update = function(xml){
 	var parser = new DOMParser();
 	var xmlDoc = parser.parseFromString(xml, "text/xml");
+	xmlDox = xmlDoc.getElementsByTagName("gameInfo")[0];
 	
 	var player = xmlDoc.getElementsByTagName("player")[0];
+	console.log(xmlDoc.getElementsByTagName("player")[0]);
 	
-	this.player.money = parseInt(player.getElementsByTagName("money")[0].nodeValue);
-	this.player.production = parseInt(player.getElementsByTagName("production")[0].nodeValue);
+	this.player.money = parseInt(player.getElementsByTagName("money")[0].textContent);
+	console.log(player.getElementsByTagName("money")[0].textContent);
+	this.player.production = parseInt(player.getElementsByTagName("production")[0].textContent);
 	this.player.items = [];
 	var items = player.getElementsByTagName("items")[0];
 	for (var i = 0; i < items.childNodes.length; i++){
 		var item = items.childNodes[i];
-		this.player.items.push(new Item(item.getElementsByTagName("name")[0].nodeValue, item.getElementsByTagName("quantity")[0].nodeValue, item.getElementsByTagName("production")[0].nodeValue));
+		this.player.items.push(new Item(item.getElementsByTagName("name")[0].textContent, item.getElementsByTagName("quantity")[0].textContent, item.getElementsByTagName("production")[0].textContent));
 	}
 	var boss = xmlDoc.getElementsByTagName("boss")[0];
-	this.boss.money = parseInt(boss.getElementsByTagName("money")[0].nodeValue);
-	this.boss.production = parseInt(boss.getElementsByTagName("production")[0].nodeValue);	
+	//this.boss.money = parseInt(boss.getElementsByTagName("money")[0].textContent);
+	this.boss.production = parseInt(boss.getElementsByTagName("production")[0].textContent);
+	this.boss.username = boss.getElementsByTagName("name")[0].textContent;
 	items = boss.getElementsByTagName("items")[0];
 	this.boss.items = [];
 	for (var i = 0; i < items.childNodes.length; i++){
 		var item = items.childNodes[i];
-		this.boss.items.push(new Item(item.getElementsByTagName("name")[0].nodeValue, item.getElementsByTagName("quantity")[0].nodeValue, item.getElementsByTagName("production")[0].nodeValue));
+		this.boss.items.push(new Item(item.getElementsByTagName("name")[0].textContent, item.getElementsByTagName("quantity")[0].textContent, item.getElementsByTagName("production")[0].textContent));
 	}
 	
 	var employees = xmlDoc.getElementsByTagName("employees")[0];
 	this.employees = [];
 	for (var i = 0; i < employees.childNodes.length; i++){
 		var employee = employees.childNodes[i];
-		this.employees.push(new Player(employee.getElementsByTagName("name")[0].nodeValue, 0, employee.getElementsByTagName("production")[0].nodeValue, []));
+		this.employees.push(new Player(employee.getElementsByTagName("name")[0].textContent, 0, employee.getElementsByTagName("production")[0].textContent, []));
 	}
 	
 	var notifications = xmlDoc.getElementsByTagName("notifications");
@@ -66,7 +70,7 @@ GameInfo.prototype.update = function(xml){
 		notifications = notifications[0];
 		for (var i = 0; i < notifications.childNodes.length; i++){
 			var notification = notifications.childNodes[i];
-			this.notifications.push(new Notification(notification.getElementsByTagName("sender")[0].nodeValue, notification.getElementsByTagName("message")[0].nodeValue));
+			this.notifications.push(new Notification(notification.getElementsByTagName("sender")[0].textContent, notification.getElementsByTagName("message")[0].textContent));
 		}
 	}
 }
